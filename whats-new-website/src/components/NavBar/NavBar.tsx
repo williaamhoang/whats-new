9f24e4f54d258ed5ddc83ffb529edfd091cf582e
williaamhoang <willhoang1@gmail.com>
2024-11-24 22:23:00 -0600

    Navbar changes, still bad

--- whats-new-website/src/components/NavBar/NavBar.tsx
+++ whats-new-website/src/components/NavBar/NavBar.tsx
@@ -6,17 +6,22 @@ const NavBar: React.FC = () => {
     return (
         <header className="navbar">
             <div className="navbar-container">
-                <div className="logo">
-                    <Link to="/">YourLogo</Link>
+                {/* Logo Section */}
+                <div className="nav-left">
+                    <div className="logo">
+                        <Link to="/">YourLogo</Link>
+                    </div>
+                    <nav className="nav-links">
+                        <Link to="/" className="nav-link">Home</Link>
+                        <Link to="/features" className="nav-link">Features</Link>
+                        <Link to="/pricing" className="nav-link">Pricing</Link>
+                    </nav>
                 </div>
-                <nav className="nav-links">
-                    <Link to="/" className="nav-links">Home</Link>
-                    <Link to="/features" className="nav-link">Features</Link>
-                    <Link to="/pricing" className="nav-link">Pricing</Link>
-                </nav>
+
+                {/* Right Section: Login and Sign Up */}
                 <div className="nav-actions">
-                    <Link to="/login" className="button button--secondary">Login</Link>
-                    <Link to="/signup" className="button button--primary">Sign-Up</Link>
+                    <Link to="/login" className="button button--secondary">Log In</Link>
+                    <Link to="/signup" className="button button--secondary">Sign Up</Link>
                 </div>
             </div>
         </header>
